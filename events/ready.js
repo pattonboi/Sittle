aef3e6d5e7470858567f5f223afe303cf3f78bd7
patton boi <computer7647@gmail.com>
2018-07-19 22:31:32 -0400

    Delete ready.js

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = () => {
-  console.log(c.bgRed.black('Alright, alright, alright, alright, alright, alright, alright, alright, alright.'));
-  client.user.setGame(`Try something like this D:`);
-};
